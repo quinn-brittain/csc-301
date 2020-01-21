aa0e989690df5aa284c722682102b4489e86193b
Quinn <qgbritt@gmail.com>
2020-01-20 21:13:42 -0500

    added asgmt-1

---
+++ asgmt-1/index.php
@@ -0,0 +1,33 @@
+<?php
+$name = "Quinn Brittain";
+$email = "brittainq1@nku.edu";
+$major = "CIT & Computer Science";
+$school_year = "2";
+$bio = "Born in California in 2000, moved to northern Kentucky in 2009. Hobbies include: codding, video gaming, emulation, and music. Interested in design, organization, and various types of technology.";
+$fun_fact = "I accidentally deleted this whole page";
+$picture = "img/quinn-brittain";
+$reasons = "Degree requirement and previous web development experience";
+$expectations = "To become more familiarized with PHP";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Assignment 1</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1><?= $name ?></h1>
+      <h1 class="display-4">It's 2:15am</h1>
+      <p class="lead">6 hours straight... I'm dead inside</p>
+    </div>
+  </div>
+</body>
+
+</html>
